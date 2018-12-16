ab079540ad48942d3075861e6964cbf1c51046b6
francisx97 <francisx97@yahoo.ie>
2018-12-16 12:15:01 +0000

    Always revert to this
    This has the claendar in the calender page

--- app/src/main/java/com/example/admin/hive/EventFragment.java
+++ app/src/main/java/com/example/admin/hive/EventFragment.java
@@ -7,11 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class EventFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_event,container,false);
+
+//        final Date today = new Date();
+//        Calendar nextYear = Calendar.getInstance();
+//        nextYear.add(Calendar.YEAR,1);
+//
+//
+//
+//        CalendarPickerView datePicker = findViewById(R.id.calendar);
+//        datePicker.init(today,nextYear.getTime()).withSelectedDate(today);
+//
+//        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+//            @Override
+//            public void onDateSelected(Date date) {
+//                String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+//                //Toast.makeText(EventFragment.this,selectedDate,Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onDateUnselected(Date date) {
+//
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/admin/hive/MainActivity.java
+++ app/src/main/java/com/example/admin/hive/MainActivity.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +23,33 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListerner);
+// setting up the main page when you open to app to display the home page
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+
+
+//setting the date for the calendar
+//        final Date today = new Date();
+//        Calendar nextYear = Calendar.getInstance();
+//        nextYear.add(Calendar.YEAR,1);
+//
+//        CalendarPickerView datePicker = findViewById(R.id.calendar);
+//        datePicker.init(today,nextYear.getTime()).withSelectedDate(today);
+//
+//        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+//            @Override
+//            public void onDateSelected(Date date) {
+//                String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+//                Toast.makeText(MainActivity.this,selectedDate,Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onDateUnselected(Date date) {
+//
+//            }
+//        });
+
 
 
     }
